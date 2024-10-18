3c2caff42b93969949b32e7f98b87dee8d3b5365
joshuades <joshua.leinweber@web.de>
2024-10-18 23:21:04 +0200

    fix: put useSearchParams() in suspense

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,12 @@
-import LoginForm from "@/components/auth/LoginForm";
+import LoginForm from '@/components/auth/LoginForm'
+import { Suspense } from 'react'
 
 export default function LoginPage() {
   return (
     <div className="container mx-auto p-4">
-      <LoginForm />
+      <Suspense>
+        <LoginForm />
+      </Suspense>
     </div>
   )
 }
