1aa673f3a69232ab2b72d6199334179dccfaf237
joshuades <joshua.leinweber@web.de>
2024-10-18 22:57:25 +0200

    clean up code

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -7,16 +7,9 @@ const HASHED_PASSWORDS = process.env.HASHED_PASSWORDS?.split(',') || []
 export async function POST(request: Request) {
   const { password } = await request.json()
 
-  console.log("POST", process.env.HASHED_PASSWORDS);
-  
   for (const hashedPassword of HASHED_PASSWORDS) {
-
-    console.log("hashedPassword", hashedPassword);
-    
+    console.log("tried hash:", hashedPassword);
     if (await verifyPassword(password, hashedPassword)) {
-
-        console.log("THIS hashedPassword", hashedPassword);
-        
       setAuthCookie(true)
       return NextResponse.json({ success: true })
     }
--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -40,7 +40,7 @@ export default function LoginForm() {
             {isLoading ? (
               <>
                 <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                Logging in...
+                Loading...
               </>
             ) : (
               'Login'
